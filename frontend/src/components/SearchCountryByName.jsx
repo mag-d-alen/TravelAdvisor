59e4b274561a10a91dcf3f92fba57713134d63b7
eytanmaden <eytanmaden2@gmail.com>
2022-01-18 01:27:03 +0200

    SearchResults and Opening/Closing modal connected

--- frontend/src/components/SearchCountryByName.jsx
+++ frontend/src/components/SearchCountryByName.jsx
@@ -7,19 +7,52 @@ import countries from '../data/countries.json';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
+import CountryProfileModal from './CountryProfileModal';
+import getCountryISO2 from 'country-iso-3-to-2';
+import axios from 'axios';
+import getToken from '../utils/getAmadeusToken';
+
 
 import FormControl from '@mui/material/FormControl';
+const uri = 'https://test.api.amadeus.com/v1/';
+
 
 export default function SearchCountryByName() {
   const [search, setSearch] = useState("");
+  const [countryCovidInfo, setCountryCovidInfo] = useState();
+  const [open, setOpen] = useState(false);
+  
+  
   // const data = countries.features.
+
+  const getCountryInfo = async (code) => {
+    const token = await getToken();
+    console.log(token);
+    const data = await axios.get(
+      `${uri}duty-of-care/diseases/covid19-area-report?countryCode=${code}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    setCountryCovidInfo(data.data.data);
+    setOpen(true);
+  };
+
+  const findCountryInfo = (e) => {
+    const countryCode = e;
+    console.log(countryCode)
+    const iso2code = getCountryISO2(countryCode);
+    getCountryInfo(iso2code);
+  };
 
   function handleChange(event){
     setSearch(event.target.value);
-    console.log(search)
   }
 
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
+    <>
     <StyledBox>
       <FormControl fullWidth sx={{ m: 1 }}>
         <InputLabel htmlFor='outlined-adornment-amount'>
@@ -33,18 +66,26 @@ export default function SearchCountryByName() {
           label='Amount'
         />
       </FormControl>
-      {countries.features.filter((val) => {if(!search){
+      {countries.features.filter((val) => {if(search.length<2){
         return null
         } else if(val.properties.ADMIN.toLowerCase().includes(search.toLowerCase())){
           return val;
         }}).map((val, key) => {
           return(
             <div key={key}>
-              <p>{val.properties.ADMIN}</p>
+              <p  onClick={(event)=>findCountryInfo(val.properties.ISO_A3)}>{val.properties.ADMIN}</p>
             </div>
           )
         })}
     </StyledBox>
+     {open && (
+        <CountryProfileModal
+          open={open}
+          handleClose={handleClose}
+          info={countryCovidInfo}
+        />
+      )}
+    </>
   );
 }
 const StyledBox = styled(Box)`
