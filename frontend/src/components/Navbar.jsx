a59ff6e6f459c411046597588a23757ec1d3049c
hutchidaughter <88189530+hutchidaughter@users.noreply.github.com>
2022-01-18 10:28:12 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,55 @@
-/** @format */
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Modal from './Modal';
 
-import React from 'react';
+const useStyles = makeStyles(theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-export default function Navbar() {
-  return <div>navbar</div>;
-}
+const Navbar = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          className={classes.menuButton}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Travel Planner
+        </Typography>
+        <Button color="inherit" onClick={handleOpen}>
+          Signup
+        </Button>
+      </Toolbar>
+      <Modal open={open} handleClose={handleClose} />
+    </AppBar>
+  );
+};
+
+export default Navbar;
